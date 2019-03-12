af5885a4b20ad92295f1dac558f3da836aa9256f
Jakob McFarland <jakob.mcfarland@digipen.edu>
2019-03-11 17:22:36 -0700

    Reimplemented Setup.

--- UtilityAI/Assets/Code/UtilityAI/BehaviorMaster.cs
+++ UtilityAI/Assets/Code/UtilityAI/BehaviorMaster.cs
@@ -2,15 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BehaviorMaster : MonoBehaviour {
+public class BehaviorMaster
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
