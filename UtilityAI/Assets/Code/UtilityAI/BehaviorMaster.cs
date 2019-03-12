40c49d78be3c77d66691ca372438e9d830f0567d
Jakob McFarland <jakob.mcfarland@digipen.edu>
2019-03-11 17:14:00 -0700

    Added IBehavior

--- UtilityAI/Assets/Code/UtilityAI/BehaviorMaster.cs
+++ UtilityAI/Assets/Code/UtilityAI/BehaviorMaster.cs
@@ -2,15 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BehaviorMaster : MonoBehaviour {
+public class BehaviorMaster
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
---
+++ UtilityAI/Assets/Code/UtilityAI/ExampleBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Code.UtilityAI
+{
+    class ExampleBehavior : IBehavior
+    {
+        public int GetPointCount()
+        {
+
+
+            return 100;
+
+        }
+    }
+}
