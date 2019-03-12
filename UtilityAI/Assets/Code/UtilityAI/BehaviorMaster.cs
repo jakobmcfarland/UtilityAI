82bcdffdf5e7febe2d08a40cbaa0350da454b078
Hubcat123 <35710805+Hubcat123@users.noreply.github.com>
2019-03-12 15:50:59 -0700

    Comented BehaviorMaster

--- UtilityAI/Assets/Code/UtilityAI/BehaviorMaster.cs
+++ UtilityAI/Assets/Code/UtilityAI/BehaviorMaster.cs
@@ -12,14 +12,17 @@ public class BehaviorMaster
     {
         behaviors.Add(behavior);
     }
+    // Returns the behavior at the given index
     public IBehavior GetBehavior(int index)
     {
         return behaviors[index];
     }
+    // Runs the action of the highest point behavior
     public void staticDecide()
     {
         List<int> pointCounts = getPointCounts();
         int maxIndex = 0, i, currMax = pointCounts[0];
+        // Finds the largest point behavior
         for (i = 1; i < pointCounts.Capacity; i++)
         {
             if (pointCounts[i] > currMax)
@@ -30,15 +33,18 @@ public class BehaviorMaster
         }
         behaviors[maxIndex].RunBehavior();
     }
+    // Randomly runs an action, with chances based on point totals
     public void probabilityDecide()
     {
         List<int> pointCounts = getPointCounts();
         int pointTotal = 0;
+        // Loops through pointCounts and adds them to pointTotal
         foreach (int i in pointCounts)
         {
             pointTotal += i;
         }
         int currPointCheck = 0, randNum = Random.Range(1, pointTotal), currIndex = 0;
+        // Loops through the points and runs the one which falls in the correct range
         foreach (int points in pointCounts)
         {
             if (randNum < points + currPointCheck)
@@ -51,9 +57,11 @@ public class BehaviorMaster
         }
         
     }
+    // Returns a list of ints with each of the behaviors
     private List<int> getPointCounts()
     {
         List<int> pointCounts = new List<int>();
+        // Loops through behaviors and adds their point values to the end of the list
         foreach (IBehavior iB in behaviors)
         {
             pointCounts.Add(iB.GetPointCount());
